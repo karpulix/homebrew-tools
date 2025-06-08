41b991d6dee8491df39c0c7ccc0a5d75e9d44f2d
Alexander Karpov <karpulix@gmail.com>
2025-06-08 04:00:42 +0400

    Update macosloginwatcher to version 1.0.30

--- macosloginwatcher.rb
+++ macosloginwatcher.rb
@@ -1,8 +1,8 @@
 class Macosloginwatcher < Formula
   desc "Monitor and notify about macOS login events via Telegram"
   homepage "https://github.com/karpulix/macosloginwatcher"
-  url "https://github.com/karpulix/macosloginwatcher/releases/download/v1.0.29/macosloginwatcher-1.0.29.tar.gz"
-  sha256 "b5841e243bc762291099591967d986dd5fa4965a4d16e84aad20b19a67636436"
+  url "https://github.com/karpulix/macosloginwatcher/releases/download/v1.0.30/macosloginwatcher-1.0.30.tar.gz"
+  sha256 "47d003eb7df42992b521b7051d847a488c2994cdbea208a8b161284a64928cd6"
   license "MIT"
 
   def install
